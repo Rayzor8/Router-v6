eaf1250d453bb7a7ec1a103fc4206c5c5de74b9c
flashback220 <rayhidayat220@gmail.com>
2021-11-05 02:39:22 +0700

    change: remove activeClassName v5 to newest ver, add new route, remove exact props v5

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import Welcome from './pages/Welcome';
 import Products from './pages/Products';
 import ProductDetail from './pages/ProductDetail';
 import MainHeader from './components/MainHeader';
+import About from './pages/About';
 
 function App() {
-  return (
-    <div>
-      <MainHeader />
-      <main>
-        <Routes>
-          <Route path="/welcome" element={<Welcome />}></Route>
-          <Route path="/products" exact element={<Products />}></Route>
-          <Route
-            path="/products/:productId"
-            element={<ProductDetail />}
-          ></Route>
-        </Routes>
-      </main>
-    </div>
-  );
+   return (
+      <div>
+         <MainHeader />
+         <main>
+            <Routes>
+               <Route path="/welcome" element={<Welcome />}></Route>
+               <Route path="/products" element={<Products />}></Route>
+               <Route path="/about" element={<About />}></Route>
+               <Route
+                  path="/products/:productId"
+                  element={<ProductDetail />}
+               ></Route>
+            </Routes>
+         </main>
+      </div>
+   );
 }
 
 export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Products</h1>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur in enim, neque, eaque minus consequuntur, recusandae officia cupiditate harum doloremque totam ad iste nobis tenetur. Eligendi asperiores voluptates est nemo.
+        </div>
+    )
+}
+
+export default About
